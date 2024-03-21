a37ea5288ff9d1f08b7131bc05147874d055e513
sihyonn <sihyeon982@naver.com>
2024-03-21 23:48:32 +0900

    feat: 배열과 튜플의 타입

--- section2/dist/chapter1.js
+++ section2/dist/chapter1.js
@@ -1,5 +1,19 @@
 // number
 let num1 = 123;
 let num2 = -123;
-let num3;
+let num3 = 0.123;
+let num4 = Infinity;
+let num5 = NaN;
+// string
+let str1 = 'hello';
+let str2 = `hello ${num1}`;
+let str3 = `hello`;
+// boolean
+let bool1 = true;
+let bool2 = false;
+// null
+let null1 = null;
+// undefined
+let unde1 = undefined;
+let numA = 10;
 export {};
