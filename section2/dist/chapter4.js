5fa4e6c98e578cb4180efb1a86b2194962a22dde
sihyonn <sihyeon982@naver.com>
2024-03-22 00:37:41 +0900

    feat: 타입별칭과 인덱스 시그니처

---
+++ section2/dist/chapter4.js
@@ -0,0 +1,13 @@
+let user1 = {
+    id: 1,
+    name: '시현',
+    nickname: '션',
+    birth: '0823',
+};
+let user2 = {
+    id: 2,
+    name: '시연',
+    nickname: '숀',
+    birth: '0825',
+};
+export {};
